c08b59a1de7252fc4b8228c966554939a0a36391
aelson <aelson.alves@gmail.com>
2024-05-12 01:33:00 -0230

    Translate properly

--- tech/aelson/algorithms/search/find_smallest/find_smallest.py
+++ tech/aelson/algorithms/search/find_smallest/find_smallest.py
@@ -6,7 +6,7 @@ class FindSmallest:
     @staticmethod
     def execute(products: List[Product], start: int, end: int) -> int:
         smallest = start
-        for atual in range(start, end + 1):
-            if products[atual].get_price() < products[smallest].get_price():
-                smallest = atual
+        for index in range(start, end + 1):
+            if products[index].get_price() < products[smallest].get_price():
+                smallest = index
         return smallest
